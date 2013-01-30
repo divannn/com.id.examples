69a324f6b20e6ce3f47bc6c6bcf1381522ac9776
divannn <unique-id@mail.ru>
2013-01-30 10:20:18 +0400

    Format

---
+++ python/gcd.py
@@ -0,0 +1,25 @@
+__author__ = 'danili'
+
+#Utils for finding GCD - Greates Common Divisor
+
+#1 by division
+def gcd1(a, b):
+	while a != 0 and b != 0:
+		if a > b:
+			a = a % b
+		else:
+			b = b % a
+	return a + b #some of them will be null here.
+
+#2 by subtraction
+def gcd2(a, b):
+	while a != 0 and b != 0:
+		if a > b:
+			a = a - b
+		else:
+			b = b - a
+	return a + b
+
+print gcd1(0,2)
+print gcd2(0,2)
+
